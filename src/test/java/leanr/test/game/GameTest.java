4d261f40460a25dca0049963b8f75fd33d21942e
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-04-24 15:03:06 -0300

    test: Add ShouldScore40IfScoresAlways2 test to verify scoring when exactly 2 pins are hit each frame

--- src/test/java/leanr/test/game/GameTest.java
+++ src/test/java/leanr/test/game/GameTest.java
@@ -13,4 +13,14 @@ public class GameTest {
         }
         assertThat(game.score()).isEqualTo(0);
     }
+
+    @Test
+    @DisplayName("Should score 40 if scores always 2 ")
+    void shouldScore40IfScoresAlways2(){
+       Game game = new Game();
+       for(int i = 0; i<20; i++){
+           game.roll(2);
+       }
+       assertThat(game.score()).isEqualTo(40);
+    }
 }
