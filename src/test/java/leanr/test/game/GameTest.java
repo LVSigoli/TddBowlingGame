3ed7229616f9b16cb8d5727a76a403854c9ec148
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-04-24 16:03:22 -0300

    refactor: Apply clean code to GameTest class

--- src/test/java/leanr/test/game/GameTest.java
+++ src/test/java/leanr/test/game/GameTest.java
@@ -16,45 +16,52 @@ void setup(){
 @Test
 @DisplayName("Should score zero in gutter game")
     void shouldScoreZeroInGutterGame() {
-        rollMany(0);
+        rollMany(20,0);
         assertThat(game.score()).isEqualTo(0);
     }
 
     @Test
     @DisplayName("Should score 40 if scores always 2 ")
     void shouldScore40IfScoresAlways2(){
-        rollMany(2);
+        rollMany(20,2);
         assertThat(game.score()).isEqualTo(40);
     }
 
     @Test
     @DisplayName("Should get bonus if spares")
     void shouldGetBonusIfSpares() {
-        game.roll(3);
+        rollSpare();
         game.roll(7);
-        game.roll(7);
-        for (int i = 0; i < 17; i++) {
-            game.roll(0);
-        }
+        rollMany( 17, 0 );
         assertThat(game.score()).isEqualTo(24);
     }
+
+
 
     @Test
     @DisplayName("Should get bonus if strikes")
     void shouldGetBonusIfStrikes() {
-        game.roll(10);
+        rollStrike();
         game.roll(5);
         game.roll(3);
-        for (int i = 0; i < 16; i++) {
-            game.roll(0);
-        }
+        rollMany(16,  0 );
         assertThat(game.score()).isEqualTo(26);
     }
 
 
-    private void rollMany(int pins){
-    for(int i =0; i<20; i++){
+
+    private void rollMany( int numberOfRolls, int pins){
+    for(int i =0; i<numberOfRolls; i++){
         game.roll(pins);
     }
     }
+    private void rollSpare() {
+        game.roll(3);
+        game.roll(7);
+    }
+
+    private void rollStrike() {
+        game.roll(10);
+    }
+
 }
