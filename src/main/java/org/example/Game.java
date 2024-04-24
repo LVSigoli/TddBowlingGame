88b9e2a98e82c181a8d129e55c2a40c67a4390eb
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-04-24 15:58:58 -0300

    refactor: Apply clean code to Game class

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Game {
 
-    private int score;
+
     private int[] rolls = new int[21];
     private int numberOfRolls;
 
@@ -12,21 +12,50 @@ public class Game {
     }
 
     public int score() {
+        int score = 0;
         int rollindex = 0;
         for (int i = 0; i < 10; i++) {
-            if(rolls[rollindex] ==10){
-                score+= 10 + rolls[rollindex+1] +rolls [rollindex+2];
+            if(isStrike(rollindex)){
+                score+= 10 + StrikeBonus(rollindex);
                 rollindex++;
             }
-            else if (rolls[rollindex] + rolls[rollindex + 1] == 10) {
-                score += 10 + rolls[rollindex+2];
+            else if (isSpare(rollindex)) {
+                score += 10 + SpareBonus(rollindex);
                 rollindex += 2;
             } else {
-                score += rolls[rollindex] + rolls[rollindex + 1];
+                score += frameScore(rollindex);
                 rollindex += 2;
             }
 
         }
         return score;
     }
+
+
+
+
+    private boolean isStrike(int rollindex) {
+        return rolls[rollindex] == 10;
+    }
+
+    private int StrikeBonus (int rollindex){
+        return rolls[rollindex+1] +rolls [rollindex+2];
+    }
+
+
+
+    private boolean isSpare(int rollindex) {
+        return rolls[rollindex] + rolls[rollindex + 1] == 10;
+    }
+
+    private int SpareBonus(int rollindex) {
+        return rolls[rollindex + 2];
+    }
+
+    private int frameScore(int rollindex) {
+        return rolls[rollindex] + rolls[rollindex + 1];
+    }
+
+
 }
+
