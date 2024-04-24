f74bd3854fbeecc6c643e16b460422df0e4cd0fa
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-04-24 15:19:50 -0300

    refactor: implement rollMany method to reduce code duplication in  tests

--- src/test/java/leanr/test/game/GameTest.java
+++ src/test/java/leanr/test/game/GameTest.java
@@ -1,29 +1,37 @@
 package leanr.test.game;
 import org.example.Game;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 public class GameTest {
 
 
+private Game game;
+@BeforeEach
+void setup(){
+    game = new Game();
+}
 
-    @Test
-    @DisplayName("Should score zero in gutter game")
+@Test
+@DisplayName("Should score zero in gutter game")
     void shouldScoreZeroInGutterGame() {
-        Game game = new Game();
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        rollMany(0);
         assertThat(game.score()).isEqualTo(0);
     }
 
     @Test
     @DisplayName("Should score 40 if scores always 2 ")
     void shouldScore40IfScoresAlways2(){
-       Game game = new Game();
-       for(int i = 0; i<20; i++){
-           game.roll(2);
-       }
-       assertThat(game.score()).isEqualTo(40);
+        rollMany(2);
+        assertThat(game.score()).isEqualTo(40);
+    }
+
+
+
+    private void rollMany(int pins){
+    for(int i =0; i<20; i++){
+        game.roll(pins);
+    }
     }
 }
