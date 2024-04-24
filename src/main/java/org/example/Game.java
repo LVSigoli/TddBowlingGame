8a05e46c2b8a2d100e5e6408ec7aa5bad6bc5f89
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-04-24 15:41:41 -0300

    feat: Implements solution to test shouldGetBonusIfSpares

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -3,13 +3,25 @@ package org.example;
 public class Game {
 
     private int score;
+    private int[] rolls = new int[21];
+    private int numberOfRolls;
 
 
-    public  void roll(int pins) {
-        score += pins;
+    public void roll(int pins) {
+        rolls[numberOfRolls++] = pins;
     }
 
-    public int score ()  {
+    public int score() {
+        int rollindex = 0;
+        for (int i = 0; i < 10; i++) {
+            if (rolls[rollindex] + rolls[rollindex + 1] == 10) {
+                score += 10 + rolls[rollindex+2];
+            } else {
+                score += rolls[rollindex] + rolls[rollindex + 1];
+            }
+            rollindex += 2;
+
+        }
         return score;
     }
 }
